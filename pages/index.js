be7d37632ae5a84075c306addd7c1530f83b737a
Barufa <56454596+fbarufa@users.noreply.github.com>
2025-05-02 15:35:53 +0000

    changing page background to pure dark color

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     
     return (
-        <div style={{ backgroundColor: '#333', color: 'white', padding: '20px', 
+        <div style={{ backgroundColor: '#000000', color: 'white', padding: '20px', 
                 textAlign: 'center', height: '100vh', display: 'flex' }}>
           <h1 style={{ fontSize: '24px' }}>Vai Corinthians!</h1>
         </div>
