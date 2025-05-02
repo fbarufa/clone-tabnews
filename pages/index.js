90b8e7ecc483c297dacb8f166adeaeb3d8087357
Barufa <56454596+fbarufa@users.noreply.github.com>
2025-05-02 15:16:24 +0000

    adding dark mode for the sake of my eyes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home(){
-    return <h1>Vai Corinthians!</h1>
+    
+    return (
+        <div style={{ backgroundColor: '#333', color: 'white', padding: '20px', textAlign: 'center' }}>
+          <h1 style={{ fontSize: '24px' }}>Vai Corinthians!</h1>
+        </div>
+      );
+
 }
 
 export default Home;
