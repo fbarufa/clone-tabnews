44161d5e48aadbc962bcf3a6ee959e03a1b2178c
Barufa <56454596+fbarufa@users.noreply.github.com>
2025-05-05 13:07:06 +0000

    centralizando texto horizontal/vertical

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-function Home(){
-    
-    return (
-        <div style={{ backgroundColor: '#000000', color: 'white', padding: '20px', 
-                textAlign: 'center', height: '100vh', display: 'flex' }}>
-          <h1 style={{ fontSize: '24px' }}>Vai Corinthians!</h1>
-        </div>
-      );
+function Home() {
+
+  return (
+    <div style={{
+      backgroundColor: '#000000',
+      color: 'white',
+      padding: '20px',
+      textAlign: 'center',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center', // Corrected for vertical centering
+      justifyContent: 'center' // Added for horizontal centering
+    }}>
+      <h1 style={{ fontSize: '96px' }}>Vai Corinthians!</h1>
+    </div>
+  );
 
 }
 
