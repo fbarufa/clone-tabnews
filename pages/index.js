d48e66dfa1d139f8632177ed8a3a6b97204d3fab
Barufa <56454596+fbarufa@users.noreply.github.com>
2025-05-12 18:26:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 function Home() {
-
   return (
-    <div style={{
-      backgroundColor: '#000000',
-      color: 'white',
-      padding: '20px',
-      textAlign: 'center',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center', // Corrected for vertical centering
-      justifyContent: 'center' // Added for horizontal centering
-    }}>
-      <h1 style={{ fontSize: '96px' }}>Vai Corinthians!</h1>
+    <div
+      style={{
+        backgroundColor: "#000000",
+        color: "white",
+        padding: "20px",
+        textAlign: "center",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center", // Corrected for vertical centering
+        justifyContent: "center", // Added for horizontal centering
+      }}
+    >
+      <h1 style={{ fontSize: "96px" }}>Vai Corinthians!</h1>
     </div>
   );
-
 }
 
 export default Home;
